4d37e35c7afd267ba108d80ac8b45baba43a3065
mermaidariadna <121664445+mermaidariadna@users.noreply.github.com>
2023-02-08 22:02:42 +0300

    Create task.hpp

---
+++ task.hpp
@@ -0,0 +1,15 @@
+#ifndef _task_hpp_
+#define _task_hpp_
+#include <iostream>
+#include <time.h>
+void swap(int, int);
+void fillRandom(int*, int, int, int);
+int showEl(const int*, int, int, int);
+int showEl2(const int*, int, int, int);
+void selectionSort(int*, int);
+int getMax(const int*, int);
+int getMin(const int*, int);
+void fillRandom100(int*, int);
+void showArray(int*, int);
+int search(int*, int, int);
+#endif /*_task_hpp_*/
